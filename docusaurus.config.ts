4b2a69d1ea5a93582f86afaf1b9684cb22688534
Lila Papazian <lilapapazian@gmail.com>
2026-07-16 22:26:06 -0300

    fix: update baseUrl in Docusaurus config for correct deployment path

--- docusaurus.config.ts
+++ docusaurus.config.ts
@@ -15,7 +15,7 @@ const config: Config = {
   url: 'https://lilapapazian.github.io',
   // Set the /<baseUrl>/ pathname under which your site is served
   // For GitHub pages deployment, it is often '/<projectName>/'
-  baseUrl: '/openlibrary-research/',
+  baseUrl: '/openlibrary-borrowing-experience/',
 
   // GitHub pages deployment config.
   // If you aren't using GitHub pages, you don't need these.
